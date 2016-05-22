e5398ec30fc671c24f91ae76f7268fe95ed1b6eb
Brandon Ros <brandonros1@gmail.com>
2016-05-22 01:00:35 -0400

    es6 down to my socks

--- index.js
+++ index.js
@@ -1,72 +1,53 @@
 'use strict';
 
-const MongoDB = require('mongodb');
+import MongoDB from 'mongodb';
 
-module.exports = {
-	init: async function (url) {
-		return await MongoDB.MongoClient.connect(url);
-	},
-	stream: function (collection, query, fields) {
-		let docs = [];
-			
-		let cursor = collection.find(query, fields);
+const db = module.exports;
 
-		return new Promise(function (resolve, reject) {
-			cursor.on('data', function (doc) {
-				docs.push(doc);
-			});
+db.ObjectId = str => MongoDB.ObjectId(str);
 
-			cursor.once('end', function () {
-				resolve(docs);
-			});
+db.init = async url => {
+	return await MongoDB.MongoClient.connect(url);
+};
+
+db.stream = (collection, query, fields) => {
+	let docs = [];
+		
+	let cursor = collection.find(query, fields);
+
+	return new Promise((resolve, reject) => {
+		cursor.on('data', doc => docs.push(doc));
+
+		cursor.once('end', () => resolve(docs));
+	});
+};
+
+db.update = async (collection, query, update, options) => {
+	let docs = await module.exports.stream(collection, query, { _id: 1 });
+	let _ids = docs.map( doc => doc._id );
+
+	return await collection.update(query, update, options)
+		.then( res => {
+			if (res.result.upserted) {
+				_ids = _ids.concat(res.result.upserted.map( r => r._id ));
+			}
+
+			return {
+				writeRes: res,
+				_ids: _ids
+			};
 		});
-	},
-	update: async function (collection, query, update, options) {
-		let _ids = (await this.stream(collection, query, { _id: 1 })).map(function (doc) {
-			return doc._id;
-		});
+};
 
-		return await collection.update(query, update, options)
-			.then(function (res) {
-				if (res.result.upserted) {
-					_ids = _ids.concat(res.result.upserted.map(function (r) {
-						return r._id;
-					}));
-				}
+db.remove = async (collection, query) => {
+	let docs = await db.stream(collection, query, { _id: 1 });
+	let _ids = docs.map( doc => doc._id );
 
-				return {
-					writeRes: res,
-					_ids: _ids
-				};
-			});
-	},
-	remove: async function (collection, query) {
-		let _ids = await this.stream(collection, query, { _id: 1 }).map(function (doc) {
-			return doc._id;
-		});
+	return await collection.remove(query).then( res => ({ writeRes: res, _ids: _ids }));
+};
 
-		return await collection.remove(query)
-			.then(function (res) {
-				return {
-					writeRes: res,
-					_ids: _ids
-				};
-			});
-	},
-	insert: async function (collection, docs) {
-		let _ids = docs.map(function (doc) {
-			return doc._id;
-		});
+db.insert = async (collection, docs) => {
+	let _ids = docs.map( doc => doc._id );
 
-		return await collection.insertMany(docs)
-			.then(function (res) {
-				return {
-					writeRes: res,
-					_ids: _ids
-				};
-			});
-	},
-	ObjectId: function (str) {
-		return MongoDB.ObjectId(str);
-	}
+	return await collection.insertMany(docs).then( res => ({ writeRes: res, _ids: _ids }));
 };
